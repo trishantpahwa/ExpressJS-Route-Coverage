9226d266a6742b5f6c464881bb54cdc6c3c82c7f
Trishant Pahwa <trishantpahwa@gmail.com>
2024-09-24 19:16:37 +0530

    Now finds nested routes also.

--- plugin.js
+++ plugin.js
@@ -6,14 +6,45 @@ function registeredRoutes(app, packageJSON) {
     console.log("Only Express 4.x is supported.");
     return [];
   }
-  const _routes = app._router.stack.filter((route) => route.name === "bound dispatch").map((route) => route.route);
-  return _routes.map((route) => {
-    let _method = Object.keys(route.methods)[0], _path = route.path, _description = "";
-    if (_path.path) {
-      _path = _path.path;
-    }
-    return `${_method.toUpperCase()}   =>   ${encodeURI(_path)}`;
-  });
+  return getRoutes(app);
+}
+
+function getRoutes(app) {
+  // Initialize the base path as empty string and start from app._router.stack
+  return app._router.stack.reduce((routes, layer) => {
+    return routes.concat(getRoutesFromLayer(layer, ''));
+  }, []);
+}
+
+function getRoutesFromLayer(layer, parentPath) {
+  // If this layer has a route, return the route's path and methods
+  if (layer.route) {
+    const path = layer.route.path;
+    const methods = Object.keys(layer.route.methods);
+    return methods.map((method) => `${method.toUpperCase()}   =>   ${encodeURI(parentPath + path)}`);
+  }
+
+  // If this layer has nested routers, process them iteratively
+  if (layer.name === 'router' && layer.handle && layer.handle.stack) {
+    const nestedPath = extractPathFromRegexp(layer.regexp);  // Convert regex to string path
+    // Reduce the nested layers (from router) to collect routes
+    return layer.handle.stack.reduce((nestedRoutes, nestedLayer) => {
+      return nestedRoutes.concat(getRoutesFromLayer(nestedLayer, parentPath + nestedPath));
+    }, []);
+  }
+
+  // If the layer doesn't match a route or router, return an empty array
+  return [];
+}
+
+// Helper function to extract the path from the layer's regexp
+function extractPathFromRegexp(regexp) {
+  const path = regexp
+    .toString()
+    .replace('/^\\/', '')  // Remove the regex start characters
+    .replace('\\/?(?=\\/|$)/i', '')  // Remove the regex end characters
+    .replace(/\\\//g, '/');  // Replace escaped slashes with regular slashes
+  return path;
 }
 
 module.exports = registeredRoutes;
